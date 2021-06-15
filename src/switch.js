8d6b49724c38d4544f85b6bea5fbe0ea7510dfb6
Samir Montalvan <montalvansamir5@gmail.com>
2021-06-15 11:05:21 -0500

    Finish Dashboard

--- src/switch.js
+++ src/switch.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./switch.css";
 
 function Switch() {
+  const ref = useRef(null);
+  function handleChange() {
+    /* console.log(ref.current.checked); */
+    if (ref.current.checked) {
+      document.body.classList.remove("is-light-mode");
+      document.body.classList.add("is-dark-mode");
+    } else {
+      document.body.classList.remove("is-dark-mode");
+      document.body.classList.add("is-light-mode");
+    }
+  }
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      ref.current.setAttribute("checked", true);
+    }
+  }, [ref]);
   return (
     <div className="toggle toggle--daynight">
       <input
+        ref={ref}
+        onChange={handleChange}
         type="checkbox"
         id="toggle--daynight"
         className="toggle--checkbox"
